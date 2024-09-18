ea02a9a69c033e5b6748abc81314181c1ef2058e
Bhagyashri-17 <140493046+Bhagyashri-17@users.noreply.github.com>
2024-09-17 23:46:21 -0800

    Update KiranaRegisterApplication.java

--- KiranaRegisterApplication.java
+++ KiranaRegisterApplication.java
@@ -14,17 +14,5 @@ public class KiranaRegisterApplication {
 
 }
 
-/*
- ********************
-Functional Requirement:-
-1. Need an API for recording transactions.
-   Transactions can be recorded in both INR and USD.
-   Refer to this API for currency conversion - https://api.fxratesapi.com/latest.
-2. An API for displaying transactions with the capability
-   to group them for daily reports.
-3. You can use a database of your choice SQL or NoSQL.
-4. Tech Stack - JAVA and SpringBoot.
- ********************
- * Refer API_Documentation file path :  resources/static/API_Documentation
- * */
+
 
